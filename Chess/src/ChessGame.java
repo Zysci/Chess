e9e211217a86caf2d0706be21dff51522b78d2de
Daniel <daniel.mena143@gmail.com>
2023-12-20 18:22:19 +0100

    Yes

---
+++ Chess/src/ChessGame.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class ChessGame {
+    public static void main(String[] args) {
+      Scanner input = new Scanner(System.in);
+      Scanner tal = new Scanner(System.in);
+      Pieces y = new Pieces();
+
+      int Produkt;
+      String x;
+      Produkt = Pieces.piece(2,3);
+
+      System.out.println("Welcome To ProtoChess, choose your piece");
+         x = input.nextLine();
+         if(x.equalsIgnoreCase("Nej")) {
+            System.out.println("Bruh moment");
+         } else {
+            System.out.println("Ayy lmao");
+         }
+         
+      System.out.println("Mata in två tal");
+      System.out.println(Produkt);
+      y.setName(x);
+      System.out.println("lmao xd");
+
+
+
+   }
+
+}
---
+++ Chess/src/Pieces.java
@@ -0,0 +1,14 @@
+public class Pieces  {
+    private int x;
+    private int y;
+
+    public static int piece(int a, int b) {
+        return(a*b);
+    }
+
+    public void setName(String name){
+        System.out.println(name);
+    } 
+
+
+}
